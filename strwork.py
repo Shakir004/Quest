56e353b2debeb8c453195557ce66444d69fe4ac4
Shakir004 <muhammedshakir0095@gmail.com>
2026-03-08 13:40:21 +0530

    update

--- strwork.py
+++ strwork.py
@@ -1,10 +1,68 @@
 """Create a string and print it."""
 
-# str = 'shakir'
-# print(str)
+#  Write a Python program to take a string from the user and print the
+    # first character using slicing.
 
-# Print each character of a string using indexing.
-# name = 'shakir'
-# print(name[::])
+# name = input("enter a string:")
+# print(name[0:1])
+
+
+# Write a program to display the last character of a string using
+    # slicing.
+
+# name = input("enter the string:")
+# print(name[-1:])
+
+
+#  Given a string, print the first 5 characters using slicing.
+
+# name  = input("enter the string:")
+# print(name[:5])
+
+# Write a program to print all characters of a string except the first one.
+
+# name = input("enter the string:")
+# print(name[1::])
+
+
+#   Write a program to print all characters of a string except the last one.
+
+# name = input("enter the string:")
+# print(name[:-1])
+
+#  Given a string, print characters from index 2 to index 6.
+
+# name = input("enter the string:")
+# print(name[1:6])
+
+# Write a program to print every character at even index positions using slicing.
+
+# name = input("enter the string:")
+# print(name[::2])
+
+# Write a program to print every character at odd index positions using slicing.
+
+# name = input("enter a string:")
+# print(name[1::2])
+
+#   Write a program to reverse a string using slicing.
+# name = input("enter a string:")
+# print(name[::-1])
+
+
+# Given a string, print the middle character(s) using slicing (assume length can be even or odd).
+
+# name = input("enter a string:")
+# length = len(name)
+# mid = length // 2
+
+# if length % 2 != 0:
+#     middle = name[mid : mid + 1]
+# else:
+#     middle = name[mid - 1 : mid + 1]
+# print(f"The middle character(s): {middle}")
+
+
+
 
 
