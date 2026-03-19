466639ceaba9b45dffbfdca4a3232d15506514a7
Shakir004 <muhammedshakir0095@gmail.com>
2026-03-19 12:40:41 +0530

    update

--- list.py
+++ list.py
@@ -108,7 +108,7 @@
 
 
 
-matrix = [[0,1,2],[3,4,5],[6,7,8]]
+# matrix = [[0,1,2],[3,4,5],[6,7,8]]
 # print(matrix)
 # for matrix in matrix:
 #     print(matrix)
@@ -120,7 +120,145 @@ matrix = [[0,1,2],[3,4,5],[6,7,8]]
 #         print(m,end=" ")
 
 
+"""append is used to add to last to the list"""
+# sample = [1,2,3,45,6,67,8]
+# print(dir(sample))
+# sample.append(3000)
+# sample.append(1,3000)
+# sample.append()
+# sample.append('quest')
+# sample.append([100,200,300])
+# print(sample)
 
+
+
+"""insert is used to insert a specific place"""
+# sample = [1,2,3,45,6,67,8]
+# sample.insert('python')
+# sample.insert(1,'python')
+# print(sample)
+
+"""pop is used to remove from want to you want to remove """
+# sample = [1,2,3,45,6,67,8]
+# sample.pop()
+# print(sample)
+
+# sample.pop(1)
+# print(sample)
+
+# sample.pop(-2)
+# print(sample)
+
+"""remove is revome the specified the value"""
+# sample = [1,2,3,45,6,67,8]
+# sample.remove()
+# print(sample)
+
+# sample.remove(3)
+# print(sample)
+
+# sample.remove(1999)
+# print(sample)
+
+
+"""clear"""
+# sample = [1,2,3,45,6,67,8]
+# sample.clear()
+# print(sample)
+
+# sample.clear(1)
+# print(sample)
+
+
+"""count is used to find how Many times to find it"""
+# sample = [1,2,3,45,6,6,67,8]
+# print(sample.count(6))
+
+
+"""copy"""
+# sample = [1,2,3,45,6,67,8]
+# copy_list = sample.copy()
+# print(copy_list)
+# copy_list[2] = 600
+# print(copy_list)
+# print(sample)
+
+# sample = [1,2,3,45,6,67,8,[100,200,3000,7]]
+# copy_list = sample.copy()
+# print(copy_list)
+
+# copy_list[-1][-1] = 700
+# print(copy_list)
+
+# print(sample)
+
+"""sort it is modify the total list"""
+# sample = [1,2,3,45,6,67,8]
+# sample.sort()
+# print(sample)
+
+# sample.sort(reverse=True)
+# print(sample)
+"""key,reverse"""
+# s = ['my','mame','is','hi']
+# s.sort()
+# print(s)
+
+# s.sort(key=len)
+# print(s)
+
+"""extent"""
+# sample = [1,2,3,45,6,67,8]
+# s = ['my','mame','is','hi']
+# s.extend(sample)
+# print(s)
+# sample.extend(s)
+# print(sample)
+
+# sample.extend('Quest in calicut')   
+# print(sample)
+
+"""index"""
+# sample = [1,2,3,45,6,67,8]
+# print(sample.index(2))
+
+# sample = [1,2,3,7,7,45,
+
+
+"""reverse function"""
+# sample = [1,2,3,45,6,67,8]
+# sample.reverse()
+# print(sample)
+
+
+"""function"""
+"""len"""
+# sample = [1,2,3,45,6,67,8]
+# print(len(sample))
+"""min"""
+# sample = [1,2,3,45,6,67,8]
+# print(min(sample))
+
+# s = ['my','mame','is','hi']
+# print(min(s))
+"""max"""
+# sample = [1,2,3,45,6,67,8]
+# print(max(sample))
+
+# s = ['my','mame','is','hi']
+# print(max(s))
+"""sum"""
+# sample = [1,2,3,45,6,67,8]
+# print(sum(sample))
+
+# s = ['my','mame','is','hi']
+# print(sum(s))
+"""sorted"""
+# s = [2883,432,5656,566]
+# sample = [1,2,3,45,6,67,8]                  #
+# print(sorted(sample))
+# s = sorted(sample)
+# print(s)
 
 
 
