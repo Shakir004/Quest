f8bef655bd0dbc141a46d7f76b8b808a1d5d7394
Shakir004 <muhammedshakir0095@gmail.com>
2026-03-24 12:59:14 +0530

    new

---
+++ listcompicantion.py
@@ -0,0 +1,63 @@
+# a = []
+# for i in range(1,11):
+#     a.append(i)
+# print(a)
+
+# n = [i for i in range(1,11)]
+# print(n)
+
+# nw =[1,2,3,4,5,6,7,8,9,10]
+# new = [i for nw in range  if n % 2==0 ]
+# print(new)
+
+# squ = [i**2 for i in range(1,11)]
+# print(squ)
+    
+
+# sq = []
+# for i in range(1,11):
+#     if i % 2==0:
+#         sq.append(i**2)
+#     else:
+#         sq.append(i**3)
+
+# print(sq)
+
+# sq =[i **2 if i % 2 == 0 else i**3 for i in range(1,11)] 
+# print(sq)
+
+"""convert to upper case"""
+# list = ['apple','orange','mango']
+# print(list)
+# a=[i.upper() for i in list]
+# print(a)
+
+# list = ['arm','mango','sound']
+# print(list)
+# a=[len(i) for i in list]
+# print(a)
+
+# list = ['apple','bn','element']
+# x =[i.upper() if i[0] in "AEIOUaeiou" else i.title() for i in list]
+# print(x)
+
+
+# list = [1,2,3,4,-6,-3,8]
+# x=[i for i in list if  i>0  ]
+# print(x)
+
+
+# str ='shakir'
+# x =[i for i in str if i in "AEIOUaeiou"]
+# print(x)
+
+
+# list1 = [1,2,3,4,5,6,7]
+# list2= [4,5,6,7,8,9]
+# x = [ i  for i in list1 and list2 if i in list1 and list2]
+# print(x)
+
+
+# s = ['1','2','3','4','string']
+# a=[int(i)for i in s if i.isdigit()]
+# print(a)
