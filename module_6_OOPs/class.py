dcdae00a059395700576c3a9472d79d8b675d71a
Shakir004 <muhammedshakir0095@gmail.com>
2026-05-12 12:54:49 +0530

    studied about oops in basic

---
+++ module_6_OOPs/class.py
@@ -0,0 +1,42 @@
+'class'
+# class student:
+#     pass
+# print(type(student))                       #<class 'type'>
+
+
+# class student:
+#     pass
+
+# richu= student()
+# print(type(richu))    #object                                #<class '__main__.student'>
+
+
+
+# class student:
+#     pass
+
+# richu =student()
+# print(id(richu))                      #memory adress=2042412500704
+
+# shakir=student()
+# print(id(student))                    #memory adress=3013197793984
+
+
+
+'methods'
+# class student:
+#     """method"""
+#     def exam(self):
+#         print('exam conducted on 8/5/2026')
+
+# """object"""
+# richu = student()
+# shakir= student()
+
+# richu.exam()                 #exam conducted on 8/5/2026
+# shakir.exam()                #exam conducted on 8/5/2026
+
+
+"""self is used to define the current parameter"""
+
+
